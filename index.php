7257ef854d50ee62d23394d6205f6c6c0d682ac7
SeriousJack666 <robert.damien999@gmail.com>
2018-05-14 18:50:34 +0200

    exos complétés

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+
+			echo "<h1>"."Exercice 1"."</h1>";
+
+				$nom = "Robert Damien" ;
+				echo "<p>"."$nom"."</p>";
+
+
+
+			echo "<h1>"."Exercice 2"."</h1>";
+
+				$nom = "Robert"; 
+				$prenom = "Damien";
+				$age = 33;
+
+				echo "<p>"."$nom"."</p>";
+				echo "<p>"."$prenom"."</p>";
+				echo "<p>"."$age"."</p>";
+
+
+
+			echo "<h1>"."Exercice 3"."</h1>";
+
+				$km = 1;
+				echo "<p>"."$km"." km"."</p>";
+
+				$km = 3;
+				echo "<p>"."$km"." km"."</p>";
+
+				$km = 125;
+				echo "<p>"."$km"." km"."</p>";
+
+
+
+			echo "<h1>"."Exercice 4"."</h1>";
+
+				$string = "yo";
+				$int = 2;
+				$float = 3.2;
+				$bool = true;
+
+				echo "<p>".$string."</p>";
+				echo "<p>".$int."</p>";
+				echo "<p>".$float."</p>";
+				echo "<p>".$bool."</p>";
+
+
+
+			echo "<h1>"."Exercice 5"."</h1>";
+
+				$int = (int)("");
+				echo "<p>".$int."</p>";
+
+				$int = 2;
+				echo "<p>".$int."</p>";
+
+
+
+			echo "<h1>"."Exercice 6"."</h1>";
+
+				$nom = "Quidam";
+				echo "<p>"."Bonjour ".$nom.", comment vas-tu?"."</p>";
+
+			echo "<h1>"."Exercice 7"."</h1>";
+
+				$nom = "Robert"; 
+				$prenom = "Damien";
+				$age = 33;
+
+				echo "<p>"."Bonjour ".$nom." ".$prenom.", tu as ".$age." ans."."</p>";				
+
+
+			echo "<h1>"."Exercice 8"."</h1>";
+
+				$opé1 = 3+4;
+				$opé2 = 5*20;
+				$opé3 = 45/5;
+
+				echo "<p>"."résultat 1 = ".$opé1."<br>"."résultat 2 = ".$opé2."<br>"."résultat 3 = ".$opé3."</p>";				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		?>
+
+
+
+
+	</body>
+</html>
